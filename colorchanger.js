d1c27a6896376cf41196d4ea49c3b23c830c1ed0
Williams Ikponmwonba <williams@Williamss-MacBook-Air.local>
2024-07-23 12:13:34 +0100

    initial commit

---
+++ colorchanger.js
@@ -0,0 +1,16 @@
+document.getElementById("change-color-btn").addEventListener("click", changeColor);
+
+function changeColor() {
+    const colorBox = document.getElementById("color-box");
+    const randomColor = getRandomColor();
+    colorBox.style.backgroundColor = randomColor;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
